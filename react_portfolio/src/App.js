7ec18f986f8245b04d94e3784093cd92b679253f
Mark Brazee <m.brazee@comcast.net>
2020-11-30 16:42:17 -0700

    moved files

--- react_portfolio/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Home from "./pages/Home";
-import Portfolio from "./pages/Portfolio";
-
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
